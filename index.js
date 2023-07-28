fa49c9a89c2c8f36d3724929f4622e664088a504
djo kangoma <djokangoma@gamil.com>
2023-07-28 17:18:13 +0100

    add: Quelques modification

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // hello word
-const sujet= ['lol','bonjour', 'pfff','silence'];
-const verbes=['courrir','danser','partir','rentrer'];
-const complement=['un rat','un chien', 'une souris'];
-function genererLesMessages
+const sujet = ['lol', 'bonjour', 'pfff', 'silence'];
+const verbe = ['courrir', 'danser', 'partir', 'rentrer'];
+const complement = ['un rat', 'un chien', 'une souris'];
+function generateTheRandomMessage(){
+    const sujetsRandom = sujet[Math.floor(Math.random() * sujet.length)];
+    const verbesRandom = verbe[Math.floor(Math.random() * verbe.length)];
+    const complementsRandom = complement[Math.floor(Math.random() * complement.length)];
+    const messageAleatoire = `${sujetsRandom}${verbesRandom} ${complementsRandom}`;
+    return messageAleatoire;
+}
+console.log(generateTheRandomMessage());
