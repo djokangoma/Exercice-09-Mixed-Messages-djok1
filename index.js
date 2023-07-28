62d6f5be696ec8b7eb984da6c70aac088302a1d8
djo kangoma <djokangoma@gamil.com>
2023-07-28 16:27:16 +0100

    my function for my program

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // hello word
-const
+const sujet= ['lol','bonjour', 'pfff','silence'];
+const verbes=['courrir','danser','partir','rentrer'];
+const complement=['un rat','un chien', 'une souris'];
+function genererLesMessages
